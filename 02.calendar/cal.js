9979e1d50c03104d84c272a937480f5323fb024d
kuma-900 <129268970+kuma-900@users.noreply.github.com>
2025-05-29 15:09:44 +0900

    カレンダープログラムの実装

---
+++ 02.calendar/cal.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+const today = new Date();
+const year = args.y || today.getFullYear();
+const month = args.m || today.getMonth() + 1;
+
+const firstDay = new Date(year, month - 1, 1);
+const lastDay = new Date(year, month, 0);
+
+const header = `${month}月 ${year}`;
+
+console.log(header.padStart(13, " "));
+console.log("日 月 火 水 木 金 土");
+
+process.stdout.write("   ".repeat(firstDay.getDay()));
+
+for (let date = 1; date <= lastDay.getDate(); date++) {
+  process.stdout.write(String(date).padStart(2, " ") + " ");
+  // 土曜日 または 月末の日なら改行する
+  if ((firstDay.getDay() + date - 1) % 7 === 6 || date === lastDay.getDate()) {
+    process.stdout.write("\n");
+  }
+}
