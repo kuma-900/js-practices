3e536475c98ffbf2826dc3df465d0f3a6771e059
kuma-900 <129268970+kuma-900@users.noreply.github.com>
2025-06-11 20:58:03 +0900

    CREATE_TABLE_SQL のフォーマットを修正（カンマ後にスペース)

--- 03.asynchronous/sql.js
+++ 03.asynchronous/sql.js
@@ -1,5 +1,5 @@
 export const CREATE_TABLE_SQL =
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)";
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)";
 export const INSERT_SQL = "INSERT INTO books (title) VALUES ('Ruby入門')";
 export const SELECT_SQL = "SELECT * FROM books WHERE id = ?";
 export const SELECT_NO_TABLE_SQL = "SELECT * FROM no_table";
